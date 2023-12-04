241d951a9694d77acdbb3c3a754184acd00bbf09
wesley-paiva-vega <wesley.snybool.dev@gmail.com>
2023-12-04 14:50:32 -0300

    mapeamento de classe entity framework

--- EasyDados/Models/Base.cs
+++ EasyDados/Models/Base.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace EasyDados.Models
 
 {
     public class Base
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int? Id { get; set; }
     }
 }
